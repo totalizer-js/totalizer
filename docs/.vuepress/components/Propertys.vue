cbb2526e45c2b0398ed28ece2413cd77e239145f
z8264 <zhangbo8264@163.com>
2020-12-17 21:45:09 +0800

    document beautify

--- docs/.vuepress/components/Propertys.vue
+++ docs/.vuepress/components/Propertys.vue
@@ -1,97 +1,40 @@
 <template>
-  <section class="demo">
-    <svg viewBox="0 0 1024 300">
-      <circle fill="#fed" cx="100" cy="80" r="40" ref="circle"/>
-      <text x="100" y="90" font-size="30" fill="#333" ref="text">el</text>
+  <div>
+    <section class="demo">
+      <h2>Sample Animation</h2>
+      <div id="div"></div>
+    </section>
+    <section class="timeline">
+      <svg viewBox="0 0 1000 250">
+        <rect x="100" y="110" rx="0" width="800" height="10" fill="#ddd" />
+        <rect x="300" y="110" rx="0" width="400" height="10" fill="#000" />
 
-      <rect x="100" y="200" rx="10" width="200" height="30" fill="#aaa" />
-      <rect x="300" y="200" rx="10" width="400" height="30" fill="#ddd" />
-      <rect x="700" y="200" rx="10" width="200" height="30" fill="#aaa" />
+        <rect
+          x="300"
+          y="110"
+          rx="0"
+          width="0"
+          height="10"
+          fill="#08c"
+          ref="rect"
+        />
 
-      <rect
-        x="100"
-        y="200"
-        rx="10"
-        width="0"
-        height="30"
-        fill="#def"
-        ref="rect1"
-      />
-      <rect
-        x="300"
-        y="200"
-        rx="10"
-        width="0"
-        height="30"
-        fill="#fed"
-        ref="rect2"
-      />
-      <rect
-        x="700"
-        y="200"
-        rx="10"
-        width="0"
-        height="30"
-        fill="#def"
-        ref="rect3"
-      />
+        <text x="200" y="165" font-size="25" fill="#333">delay</text>
+        <text x="500" y="165" font-size="25" fill="#333">duration</text>
+        <text x="800" y="165" font-size="25" fill="#333">endDelay</text>
 
-      <text x="200" y="223" font-size="20" fill="#333">1000</text>
-      <text x="500" y="223" font-size="20" fill="#333">2000</text>
-      <text x="800" y="223" font-size="20" fill="#333">1000</text>
-
-      <text x="200" y="265" font-size="25" fill="#fff">delay</text>
-      <text x="500" y="265" font-size="25" fill="#fff">duration</text>
-      <text x="800" y="265" font-size="25" fill="#fff">endDelay</text>
-
-      <!-- <line
-        x1="100"
-        y1="170"
-        x2="100"
-        y2="260"
-        stroke="#aaa"
-        stroke-width="4"
-        stroke-dasharray="10 2"
-      />
-      <line
-        x1="900"
-        y1="170"
-        x2="900"
-        y2="260"
-        stroke="#aaa"
-        stroke-width="4"
-        stroke-dasharray="10 2"
-      />
-      <line
-        x1="300"
-        y1="170"
-        x2="300"
-        y2="260"
-        stroke="#aaa"
-        stroke-width="4"
-        stroke-dasharray="10 2"
-      />
-      <line
-        x1="600"
-        y1="170"
-        x2="600"
-        y2="260"
-        stroke="#aaa"
-        stroke-width="4"
-        stroke-dasharray="10 2"
-      /> -->
-
-      <line
-        x1="100"
-        y1="170"
-        x2="100"
-        y2="260"
-        stroke="#fed"
-        stroke-width="4"
-        ref="line"
-      />
-    </svg>
-  </section>
+        <line
+          x1="100"
+          y1="70"
+          x2="100"
+          y2="160"
+          stroke="#08c"
+          stroke-width="4"
+          ref="line"
+        />
+      </svg>
+    </section>
+  </div>
 </template>
 <script>
 import Totalizer from "../../../src/Totalizer";
@@ -101,91 +44,73 @@ export default {
     return {};
   },
   mounted() {
-    const animation = new Totalizer();
+    const t = new Totalizer();
 
-    animation.add({
-      el: this.$refs.circle,
+    t.add({
+      el: document.getElementById("div"),
       props: {
-        cx: [100, 900],
+        translateX: [0, 270],
       },
-      delay:1000,
-      endDelay:1000,
-      duration: 2000,
-      easing: 'easeOutBounce',
+      delay: 500,
+      endDelay: 500,
+      duration: 1000,
+      easing: "easeOutBounce",
     });
 
-    animation.add({
-      el: this.$refs.text,
-      props: {
-        x: [100, 900],
-      },
-      delay:1000,
-      endDelay:1000,
-      duration: 2000,
-      easing: 'easeOutBounce',
-    });
-
-    animation.add({
+    t.add({
       el: this.$refs.line,
       props: {
         x1: [100, 900],
         x2: [100, 900],
       },
-      duration: 4000,
+      duration: 2000,
     });
 
-    animation.add({
-      el: this.$refs.rect1,
-      props: {
-        width: [0, 200],
-      },
-      delay: 0,
-      duration: 1000,
-    });
-    animation.add({
-      el: this.$refs.rect2,
+    t.add({
+      el: this.$refs.rect,
       props: {
         width: [0, 400],
       },
-      delay: 1000,
-      duration: 2000,
-    });
-    animation.add({
-      el: this.$refs.rect3,
-      props: {
-        width: [0, 200],
-      },
-      delay: 3000,
+      delay: 500,
       duration: 1000,
     });
 
-    animation.loop().alternate().play();
+    t.loop().alternate().play();
   },
   methods: {},
 };
 </script>
 <style scoped>
 .demo {
-  margin-top:10px;
+  margin-top: 10px;
   position: relative;
   height: 300px;
   max-width: 400px;
   background: #f1f1f1;
   border-radius: 10px;
   border: 2px solid #fff;
-  box-shadow: 0 1px 2px rgba(0,0,0,.5);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.5);
+}
+.timeline {
+  margin-top: 10px;
+  position: relative;
+  max-width: 400px;
+  background: #f1f1f1;
+  border-radius: 10px;
+  border: 2px solid #fff;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.5);
 }
 h2 {
-  position:absolute;
-  padding:0;
-  margin:0;
-  right:15px;
-  top:0px;
-  line-height:40px;
-  color:#08c;
-  font-size:16px;
+  position: absolute;
+  padding: 0;
+  margin: 0;
+  right: 15px;
+  top: 0px;
+  line-height: 40px;
+  color: #08c;
+  font-size: 16px;
   font-weight: normal;
-  border:none;
+  border: none;
   text-shadow: 1px 1px 0 #fff;
 }
 svg {
@@ -193,7 +118,18 @@ svg {
   width: 100%;
 }
 text {
-  text-anchor: middle;
+  text-anchor:middle;
+}
+#div {
+  position: absolute;
+  top: 50%;
+  left: 50px;
+  width: 30px;
+  height: 20px;
+  margin-top: -10px;
+  background: #08c;
+  border-radius: 4px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.5);
 }
 </style>>
 
--- docs/document/Property.md
+++ docs/document/Property.md
@@ -1,6 +1,6 @@
 # 动画描述
 
-Demo
+## 单个动画描述
 
 <ClientOnly>
   <Propertys/>
@@ -9,16 +9,64 @@ Demo
 CODE EXAMPLE
 
 ``` javascript
-{
-  el: document.getElementById('svgCircle'),
+const t = new Totalizer();
+t.add({
+  el: document.getElementById("div"),
   props: {
-    cx: [100, 900],
+    translateX: [0, 270],
+  },
+  delay: 500,
+  endDelay: 500,
+  duration: 1000,
+  easing: "easeOutBounce",
+});
+```
+
+## 多个动画描述
+
+<ClientOnly>
+  <Propertys02/>
+</ClientOnly>
+
+
+``` javascript
+const t = new Totalizer();
+const el = document.getElementById("div");
+t.add({
+  el,
+  props: {
+    translateX: [0, 270],
+  },
+  duration: 2000,
+});
+
+t.add({
+  el,
+  props: {
+    scale: [1, 2],
+  },
+  delay:250,
+  duration: 500,
+});
+
+t.add({
+  el,
+  props: {
+    scale: [2, 1],
+  },
+  delay:750,
+  duration: 500,
+});
+
+t.add({
+  el,
+  props: {
+    translateY: [0, 200],
   },
   delay:1000,
-  endDelay:1500,
-  duration: 1500,
-  easing: 'easeOutBounce',
-}
+  duration: 750,
+  easing: "easeOutBounce",
+});
 ```
 
 ## API
